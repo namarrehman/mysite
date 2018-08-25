7abf007493bc7253602997cf256c8e7d0c61f913
namarrehman <namarrehman@gmail.com>
2018-08-25 14:43:44 +0300

    updated url file as per page #24

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),('published', 'Published'),)
@@ -24,3 +26,9 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+            args=[self.publish.year,
+                self.publish.strftime('%m'),
+                    self.publish.strftime('%d'),
+                        self.slug])
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include, url
-from django.urls import path
 
+'''
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
@@ -25,4 +26,3 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
 ]
-'''
